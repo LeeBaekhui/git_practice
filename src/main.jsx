c7027d456287f306cad7f04ea20b5e2fb2796d04
alee202 <alee202@naver.com>
2024-05-23 10:09:09 +0900

    메시지

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MiniVariantDrawer from "./components/layouts/MiniVariantDrawer";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MiniVariantDrawer />
+  </React.StrictMode>
+);
